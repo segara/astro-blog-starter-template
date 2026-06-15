f1f779a0807e36d781225bdbe0298110ca6228ee
segara <segara@com2us.com>
2026-06-15 22:31:44 +0900

    font change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,8 +17,8 @@ module.exports = {
         info: "rgb(var(--color-info) / <alpha-value>)",
       },
       fontFamily: {
-        sans: ["'Inter Tight Variable'", "Helvetica", "Verdana", "sans-serif"],
-        body: ["'Inter Tight Variable'", "Helvetica", "Verdana", "sans-serif"],
+        sans: ["'MyFont'", "Helvetica", "Verdana", "sans-serif"],
+        body: ["'MyFont'", "Helvetica", "Verdana", "sans-serif"],
       },
       boxShadow: {
         inset: " inset 2px 2px 40px -20px rgba(0, 0, 0, 0.3)",
